72481c0634f6c5af058932b5538c7dd72f1a6736
Catherine <codingk8@gmail.com>
2018-12-16 20:24:45 +0100

    Create 51_keep_hydrated.js

---
+++ 51_keep_hydrated.js
@@ -0,0 +1,12 @@
+/* xxx */
+
+// My first solution
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+// ES6
+const litres = time => Math.floor(time * 0.5);
+
+
+/* That's all folks! */
