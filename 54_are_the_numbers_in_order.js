7478a5e1fa6cfe18ca347243885bebb41aa04969
Catherine <codingk8@gmail.com>
2019-01-23 20:07:18 +0100

    Update 54_are_the_numbers_in_order.js

--- 54_are_the_numbers_in_order.js
+++ 54_are_the_numbers_in_order.js
@@ -1,1 +1,37 @@
-// xxx
+// Super simple en fait
+
+function inAscOrder(arr) {
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] >= arr[i+1]) { 
+      return false;
+    }    
+}
+return true; 
+}
+
+// Mais j'avais fait ça, pour explorer la fonction every
+
+function inAscOrder(arr) {
+  let s = arr.join(", ");
+  let a = [];
+  let result;
+ 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < arr[i+1]) {
+        a.push(true);
+    } else if (arr[i] > arr[i+1]) {
+        a.push(false);
+    }
+  }
+  
+  if (a.every(item => item === true)) {
+    return 'The list of numbers "' + s + '" is in ascending order';
+  } else if (a.every(item => item === false)) {
+    return 'The list of numbers "' + s + '" is in DESCENDING order';
+  } else {
+    return 'The list of numbers "' + s + '" is not in ascending order';
+  } 
+}
+
+
+// This is it ! :-)
